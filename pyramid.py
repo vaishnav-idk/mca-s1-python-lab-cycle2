8e124c50daadb0da450ae5e1ca1b6261d3cbc751
vaishnav-idk <63421367+vaishnav-idk@users.noreply.github.com>
2022-11-25 09:30:58 +0530

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,9 +1,9 @@
 def pyramid():
     n=int(input("Enter a value "))
     for i in range (0,n+1):
-        j=0
+        
         a=0
-        for j in range(n):
+        for j in range(i):
             a=a+i
             print(a,end=" ")
             
